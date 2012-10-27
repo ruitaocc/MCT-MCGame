fddc60360bdff3d79d5f564b5e5119c70b5a8a8e
ruitaocc <ruitaocc@gmail.com>
2012-10-27 13:27:17 +0800

    20121027

---
+++ Classes/sceneController.h
@@ -0,0 +1,13 @@
+//
+//  sceneController.h
+//  MCGame
+//
+//  Created by kwan terry on 12-10-25.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface sceneController : NSObject
+
+@end
---
+++ Classes/sceneController.m
@@ -0,0 +1,13 @@
+//
+//  sceneController.m
+//  MCGame
+//
+//  Created by kwan terry on 12-10-25.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "sceneController.h"
+
+@implementation sceneController
+
+@end
