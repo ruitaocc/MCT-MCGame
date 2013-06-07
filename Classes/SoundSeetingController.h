f8a185262afaa83a558a96f97dd3fbedbd34b738
ruitaocc <ruitaocc@gmail.com>
2013-06-07 10:04:23 +0800

    fix some bug!

---
+++ Classes/SoundSeetingController.h
@@ -0,0 +1,13 @@
+//
+//  SoundSeetingController.h
+//  MCGame
+//
+//  Created by kwan terry on 13-6-6.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SoundSeetingController : NSObject
+
+@end
---
+++ Classes/SoundSeetingController.m
@@ -0,0 +1,13 @@
+//
+//  SoundSeetingController.m
+//  MCGame
+//
+//  Created by kwan terry on 13-6-6.
+//
+//
+
+#import "SoundSeetingController.h"
+
+@implementation SoundSeetingController
+
+@end
